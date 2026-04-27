9166c2ef1d60e0d8cef48413fbc38e34110765ac
Samiksha Jain <samikshaj7371@gmail.com>
2026-04-28 00:27:26 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
